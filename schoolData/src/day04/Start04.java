487a7805b54924156bf4d3be67058337ef1eb4d1
user <user@user-PC>
2019-10-15 10:08:18 +0900

    create repo

---
+++ schoolData/src/day04/Start04.java
@@ -0,0 +1,14 @@
+package day04;
+
+public class Start04 {
+
+	public Start04() {
+		System.out.println("Hello World!!!");
+	}
+
+	public static void main(String[] args) {
+		new Start04();
+
+	}
+
+}
